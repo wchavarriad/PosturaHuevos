c018a18147297ab117eba26c3627199ca579e6d4
wchavarriad <34863481+wchavarriad@users.noreply.github.com>
2025-05-28 04:26:17 +0000

    verion funcional unificada

--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def cargar_datos():
-    df = pd.read_csv("data/control_produccion_huevos_actualizado.csv")
-    df["Fecha"] = pd.to_datetime(df["Fecha"], errors="coerce")
-    df["Total (Manuscrito)"] = df["Total (Manuscrito)"].astype(str).str.replace(",", "").astype(float)
-    return df.sort_values("Fecha").reset_index(drop=True)
